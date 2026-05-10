1edbc1f5c9171c81ae04a5ec5279a019ca971292
detskiyded <dodustester@gmail.com>
2026-05-10 23:02:14 +0400

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express, {Request, Response} from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/api/health', (req: Request, res: Response) => {
+    res.status(200).json({
+        status: 'ok',
+        message: 'its alive!',
+        timestamp: new Date().toISOString() 
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+    console.log(`http://localhost:${PORT}/api/health`);
+})
